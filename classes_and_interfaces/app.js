ccf7478ee53f2a88a2407153a4466890be6af636
Omkar Adhali <omkar.a2989@gmail.com>
2023-08-14 07:26:44 -0400

    classes and properties

--- classes_and_interfaces/app.js
+++ classes_and_interfaces/app.js
@@ -1,1 +1,29 @@
+"use strict";
 console.log("Hello from TS");
+class Department {
+    constructor(n) {
+        // making properties private makes them accessible only inside class.
+        this.employees = [];
+        this.name = n;
+    }
+    describe() {
+        console.log("Department: " + this.name);
+    }
+    // This is interpreted as a hint. When describeNew is executed then this parameters should always be of type Department.
+    describeNew() {
+        console.log("Department: " + this.name);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInfo() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+const accounting = new Department("Accounting");
+console.log(accounting);
+accounting.describe();
+accounting.addEmployee("max");
+accounting.addEmployee("Ins");
+accounting.printEmployeeInfo();
--- classes_and_interfaces/app.ts
+++ classes_and_interfaces/app.ts
@@ -1,1 +1,41 @@
 console.log("Hello from TS");
+
+class Department {
+  name: string;
+
+  // making properties private makes them accessible only inside class.
+  private employees: string[] = [];
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe() {
+    console.log("Department: " + this.name);
+  }
+
+  // This is interpreted as a hint. When describeNew is executed then this parameters should always be of type Department.
+  describeNew(this: Department) {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const accounting = new Department("Accounting");
+
+console.log(accounting);
+
+accounting.describe();
+
+accounting.addEmployee("max");
+accounting.addEmployee("Ins");
+
+accounting.printEmployeeInfo();
